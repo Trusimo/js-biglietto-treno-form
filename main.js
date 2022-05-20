b8e1db67d85a2de9ccb9d6020e8ca5814e034cfe
Trusimo <simonetrunfio96@gmail.com>
2022-05-20 10:16:38 +0200

    put variables in listener

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-var nome = document.getElementById("username").value;
-var eta = document.getElementById("userage").value;
-var chilometri = document.getElementById("userkm").value;
-
-var submit = document.getElementById("submit-btn");
+const submit = document.getElementById("submit-btn");
 
 submit.addEventListener("click", function() {
+
+    var nome = document.getElementById("username").value;
+    var eta = document.getElementById("userage").value;
+    var chilometri = document.getElementById("userkm").value;
+    var carrozza = Math.floor(Math.random() * 11);
+    var numerobiglietto = Math.floor(Math.random() * 101);
+
     if (eta < 17) {
         var prezzo = 0.168 * (chilometri);
     } else if (eta > 65) {
@@ -12,6 +15,9 @@ submit.addEventListener("click", function() {
     } else {
         var prezzo = 0.21 * (chilometri);
     }
+
     document.getElementById("nome").innerHTML = (nome);
-    document.getElementById("prezzo").innerHTML = (prezzo);
+    document.getElementById("prezzo").innerHTML = (prezzo).toFixed(2);
+    document.getElementById("carrozza").innerHTML = (carrozza);
+    document.getElementById("numerobiglietto").innerHTML = (numerobiglietto)
 })
